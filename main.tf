a0bd13b00ad274b5402141612a710a14dc3c077f
gabriel.goliveira@sp01.local <gabriel.goliveira@totvs.com.br>
2024-07-06 15:12:48 -0300

    finalizacao do curso

--- main.tf
+++ main.tf
@@ -17,7 +17,13 @@ resource "aws_instance" "app_server" {
     ami = "ami-0139963043080810f"
     instance_type = "t2.micro"
     key_name = "ubuntu-20.04-aws-keypair"
+    # user_data = <<-EOF
+    #             #!/bin/bash
+    #             cd /home/ubuntu
+    #             echo "<h1>Feito com Terrform</h1>" > index.html
+    #             nohup busybox httpd -f -p 8080 &
+    #             EOF
     tags = {
-        Name = "Primeira instancia"
+        Name = "Terraform Ansible Python"
     }
 }
